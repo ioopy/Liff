07a120305a9b645406685721511ac304e51d15b1
ioopy <31724142+ioopy@users.noreply.github.com>
2021-08-08 17:33:55 +0700

    Update products.js

--- products.js
+++ products.js
@@ -1,25 +1,25 @@
 // DUMMY PRODUCTS (PRODUCT ID : DATA)
 var products = {
   123: {
-    name : "pork",
+    name : "หมู",
     desc : "1 kg",
     img : "https://static.thairath.co.th/media/dFQROr7oWzulq5FZUEh1bGHbkAlMP6YU69FzlfmtDtIvKULTA65Qg2Y02blCtbVGNLp.jpg",
     price : 100
   },
   124: {
-    name : "pork",
+    name : "หมู",
     desc : "1.5 kg",
     img : "https://static.thairath.co.th/media/dFQROr7oWzulq5FZUEh1bGHbkAlMP6YU69FzlfmtDtIvKULTA65Qg2Y02blCtbVGNLp.jpg",
     price : 120
   },
   125: {
-    name : "pork",
+    name : "หมู",
     desc : "0.5 kg",
     img : "https://static.thairath.co.th/media/dFQROr7oWzulq5FZUEh1bGHbkAlMP6YU69FzlfmtDtIvKULTA65Qg2Y02blCtbVGNLp.jpg",
     price : 70
   },
   126: {
-    name : "meet",
+    name : "เนื้อ",
     desc : "0.5 kg",
     img : "https://static.thairath.co.th/media/dFQROr7oWzulq5FZUEh1bGHbkAlMP6YU69FzlfmtDtIvKULTA65Qg2Y02blCtbVGNLp.jpg",
     price : 120
