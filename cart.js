11014a122d74ffcd0c28a67c0e8791a281ff5f13
ioopy <31724142+ioopy@users.noreply.github.com>
2021-08-08 16:27:24 +0700

    Update cart.js

--- cart.js
+++ cart.js
@@ -266,6 +266,37 @@ var cart = {
                                 ],
                                 margin: "lg",
                             },
+                            {
+                                type: "box",
+                                layout: "horizontal",
+                                contents: [
+                                    {
+                                        type: "box",
+                                        layout: "vertical",
+                                        contents: [
+                                            {
+                                                type: "text",
+                                                text: "P ioopy",
+                                                size: "sm",
+                                            },
+                                        ],
+                                        width: "110px",
+                                    },
+                                    {
+                                        type: "box",
+                                        layout: "vertical",
+                                        contents: [
+                                            {
+                                                type: "text",
+                                                text: "P ioopy",
+                                                size: "sm",
+                                                align: "end",
+                                                weight: "bold",
+                                            },
+                                        ],
+                                    },
+                                ],
+                            },
                         ],
                         spacing: "md",
                     },
